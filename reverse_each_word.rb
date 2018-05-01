72c87b64cb687762ba2855b6641c35d946389e61
Eric Chrobak <eric@ericchrobak.com>
2018-05-01 01:07:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,10 @@
-require 'pry'
 def reverse_each_word(string)
   array = string.split(" ")
   phrase = ""
   reversed_word = ""
   array.each do |word|
-    binding.pry
     reversed_word = word.reverse! + " "
     phrase = phrase + reversed_word
   end
-  return phrase
+  phrase
 end
