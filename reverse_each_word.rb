2ecd053acad4753febab00019e67f5ac1d2788e6
Eric Chrobak <eric@ericchrobak.com>
2018-05-01 01:13:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,15 +6,9 @@ def reverse_each_word(string)
     reversed_word = word.reverse! + " "
     phrase = phrase + reversed_word
   end
-  phrase.chop
-end
-
-def reverse_each_word(string)
-  array = string.split(" ")
-  phrase = ""
-  reversed_word = ""
   array.collect do |word|
     reversed_word = word.reverse! + " "
     phrase = phrase + reversed_word
   end
+  phrase.chop
 end
