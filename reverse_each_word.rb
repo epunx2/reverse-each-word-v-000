77491683a714c9c7e8dcea8bbbcb358f78f8be35
Eric Chrobak <eric@ericchrobak.com>
2018-05-01 01:12:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,13 @@ def reverse_each_word(string)
   end
   phrase.chop
 end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  phrase = ""
+  reversed_word = ""
+  array.collect do |word|
+    reversed_word = word.reverse! + " "
+    phrase = phrase + reversed_word
+  end
+end
